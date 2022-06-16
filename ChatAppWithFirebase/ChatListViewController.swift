d2e631801b8bc66d137eb01faf3052914a3a3306
Ochanyoro <wadaoo0223@icloud.com>
2022-06-16 15:12:21 +0900

    create ChatList.storyboard and ChatListViewController, put a initialized screen

---
+++ ChatAppWithFirebase/ChatListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChatListViewController.swift
+//  ChatAppWithFirebase
+//
+//  Created by 大和田一裕 on 2022/06/16.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
